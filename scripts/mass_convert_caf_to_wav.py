95964bb8043fc84de7b574e3df48bce60c32aaaa
0xfrogz <136229101+0xfrogz@users.noreply.github.com>
2024-11-04 17:02:07 +0100

    wip- mass convert caf to wav

--- scripts/mass_convert_caf_to_wav.py
+++ scripts/mass_convert_caf_to_wav.py
@@ -1,28 +1,32 @@
 import os
-import ffmpeg
+import pprint
 
-#root_folder = r'E:\music prod\sample packs\apple_loops'
-#copy_to_folder = r'E:\music prod\sample packs\apple_loops\0_converted'
-
-## testing 
-root_folder = r'E:\music prod\sample packs\apple_loops\test'
+root_folder = r'E:\music prod\sample packs\apple_loops'
 copy_to_folder = r'E:\music prod\sample packs\apple_loops\0_converted'
 
-to_convert = []
-#loop through each folder in root_folder
+# {'folder_name': ['file1', 'file2', 'file3']}
+file_dict = {}
+
 for folder in os.listdir(root_folder):
     cur_folder = os.path.join(root_folder, folder)
-    cur_folder_dict = {}
 
     if os.path.isdir(cur_folder):
+        file_dict[folder] = []
         for file in os.listdir(cur_folder):
-            if file.endswith('.wav'):
-                print(file)
-            if file.endswith('.caf'):
-                # add file to cur_folder_dict with key folder and value file
-                cur_folder_dict[folder] = file
-                to_convert.append(cur_folder_dict)
+            cur_file_path = os.path.join(cur_folder, file)
+            if os.path.isfile(cur_file_path):
+                file_dict[folder].append(cur_file_path)
 
-    print(cur_folder_dict)
+""""
+# now go through dict and convert if wav.
+# otherwise we just move to target folder as is
+for sound_pack in file_dict:
+    for sound_file in file_dict[sound_pack]:
+        if sound_file.endswith('.wav'):
+            print(sound_file)
+        if sound_file.endswith('.caf'):
+            # add file to cur_folder_dict with key folder and value file
+            file_dict[sound_pack].append(sound_file)
+"""
 
-
+pprint.pprint(file_dict)
